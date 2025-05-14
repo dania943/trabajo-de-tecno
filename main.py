7b40dc22ba3c4089b3e23de568cb99cb1e5790b6
dania943 <daniayanarico550@gmail.com>
2025-05-14 11:08:41 +0000

    mensaje oculto

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+variable12=17
+variable11="dania"
+variable99=True
+variable10=None
+pi=-7.9
+print(pi) 
+
 variable=17
-variable="dania"
-variable=true#boolean
-variable=None
-print(variable)#None
+comparacion=variable==17
+print(comparacion)
+comparacion=not comparacion
+print(comparacion)
+mayor=variable>=0
+print(mayor)
+menor=variable<=0
+print(menor)
+
+mayor=(1+1)>1
+menor=(20-30)<10
+y=mayor and menor
+print(y,"and")
+
+
+
+
