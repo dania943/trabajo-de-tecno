a2ae28e59078c45b7a1b5ad2240640194d98ab19
dania943 <daniayanarico550@gmail.com>
2025-08-06 11:54:56 +0000

    6/8

--- 28/act7.py
+++ 28/act7.py
@@ -1,1 +1,16 @@
 #clase 6 e agosto del 2025
+class clase:
+    def __init__(self,atributo):
+        #init va con dos guiones bajos al inicio y al final
+        self.atributo = atributo
+
+    def metodo(self):
+        return f"El atributo es: " + self.atributo
+
+    def imprimir(self):
+        print("imprimiendo:" + self.atributo)
+    
+instancia = clase("valor") 
+print(instancia.metodo() )  
+instancia.imprimir() 
+#crear una clase que sume dos numeros y los muestre por pantalla
